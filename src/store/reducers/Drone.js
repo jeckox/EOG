737eb6a9446692c83ff3528365d080923af66924
Adrián Aburto <jeckox@gmail.com>
2019-06-05 17:17:10 -0500

    recharts implemented and graph visualization every 4 seconds

--- src/store/reducers/Drone.js
+++ src/store/reducers/Drone.js
@@ -2,44 +2,35 @@ import * as actions from "../actions";
 
 const initialState = {
     loading: false,
-    // loading: false,
-    // weatherId: null,
-    // name: "",
-    // temperature: "",
-    // weather_state_name: "",
-    // latitude: null,
-    // longitude: null,
-    data: []
+    firstTime: true,
+    datas: []
 };
 
-//const toF = c => (c * 9) / 5 + 32;
-
+const addZ = n => (n <= 9 ? '0' + n : n);
+const toDate = theTime => {
+    let theOriginalDate = new Date(theTime);
+    let yearDate = theOriginalDate.getFullYear() + '-' + addZ(theOriginalDate.getMonth() + 1) + '-' + addZ(theOriginalDate.getDate());
+    let timeDate = addZ(theOriginalDate.getHours() + 1) + ':' + addZ(theOriginalDate.getMinutes() + 1) + ':' + addZ(theOriginalDate.getSeconds() + 1);
+    return yearDate + ' ' + timeDate;
+}
 const startLoading = (state, action) => {
     return {...state, loading: true };
 };
 
 const droneDataRecevied = (state, action) => {
     const { data } = action.data;
+    let graphData;
     if (!data.length) return state;
-    //console.info(data);
-    // const weather = data.consolidated_weather[0];
-    // const { weather_state_name, the_temp } = weather;
-    // const { latt_long, title: name } = data;
-    // const [latitude, longitude] = latt_long.split(",");
+    graphData = data.map(function(d) {
+        return { x: toDate(d.timestamp), y: d.metric }
+    });
+
 
     return {
         ...state,
         loading: false,
-        data: action.data
-            // ...state,
-            // loading: false,
-            // latitude,
-            // longitude,
-            // temperatureinCelsius: the_temp,
-            // temperatureinFahrenheit: toF(the_temp),
-            // weather_state_name,
-            // name,
-            // data: action.data
+        firstTime: false,
+        datas: graphData
     };
 };
 
